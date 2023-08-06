31392d0567d76bbadee7e9ffb10398406fa014f2
MirkoRajovicSY <mirko.rajovic@symphony.is>
2023-08-06 23:36:47 +0200

    Update middleware response json

--- router_edu/utils/requestValidationMiddleware.js
+++ router_edu/utils/requestValidationMiddleware.js
@@ -1,12 +1,40 @@
 const { validationResult } = require("express-validator");
 
+const generateErrorResponse = (errors) => {
+  return errors.array().reduce((acc, error) => {
+    if (!acc[error.path]) {
+      return {
+        ...acc,
+        [error.path]: {
+          type: error.type,
+          field: error.path,
+          value: error.value,
+          message: error.msg,
+          messages: [error.msg],
+        },
+      };
+    } else {
+      return {
+        ...acc,
+        [error.path]: {
+          ...acc[error.path],
+          messages: [...acc[error.path].messages, error.msg],
+        },
+      };
+    }
+  }, {});
+};
+
 const requestValidationMiddleware = (validations) => {
   return async (req, res, next) => {
     await Promise.all(validations.map((validation) => validation.run(req)));
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      const errorsCustom = generateErrorResponse(errors);
+      return res.status(400).json({ errors: errorsCustom });
     }
+
     next();
   };
 };
